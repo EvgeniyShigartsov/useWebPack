e2d9cb7a156b7bc91a0c4107566ec14a3ee88255
Evgeniy Shygartsov <evgeniy@MacBook-Air.local>
2022-01-10 00:28:54 +0200

    update webpack magic comments

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,15 +3,15 @@ import React from 'react'
 const routes = {
   Home: {
     path: '/',
-    component: React.lazy(() => import('../components/Home' /* webpackChunkName Home */)),
+    component: React.lazy(() => import('../components/Home' /* webpackChunkName: "Home" */)),
   },
   About: {
     path: '/about',
-    component: React.lazy(() => import('../components/About' /* webpackChunkName About */)),
+    component: React.lazy(() => import('../components/About' /* webpackChunkName: "About" */)),
   },
   Users: {
     path: '/users',
-    component: React.lazy(() => import('../components/Users' /* webpackChunkName Users */)),
+    component: React.lazy(() => import('../components/Users' /* webpackChunkName: "Users" */)),
   },
 }
 
